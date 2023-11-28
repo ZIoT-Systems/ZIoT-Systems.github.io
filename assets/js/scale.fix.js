91070f185679d48ff6eed2a31da9e996bfb952b0
jglatts <john@z-axiscc.com>
2023-11-28 16:06:15 -0500

    ios fix

--- assets/js/scale.fix.js
+++ assets/js/scale.fix.js
@@ -8,23 +8,21 @@
             }
         },
         initialize = function() {
-            changeViewportContent("width=device-width, minimum-scale=1.0, maximum-scale=1.0");
+            changeViewportContent("width=device-width, minimum-scale=0.25, maximum-scale=1.0");
         },
         gestureStart = function() {
-            changeViewportContent("width=device-width, minimum-scale=0.25, maximum-scale=1.6");
+            changeViewportContent("width=device-width, minimum-scale=0.25, maximum-scale=1.0");
         },
         gestureEnd = function() {
             initialize();
         };
 
-
-    /*
     if (navigator.userAgent.match(/iPhone/i)) {
         initialize();
         document.addEventListener("touchstart", gestureStart, false);
         document.addEventListener("touchend", gestureEnd, false);
     }
-    */
+
 })(document);
 
 function myFormFunc() {
