c66e493b6431328745775027ea3f5154da86c89f
jglatts <john@z-axiscc.com>
2024-08-28 13:57:04 -0400

    updates Wed 08/28/2024 13:57:04.42

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -2,6 +2,6 @@ console.log("hello from loaded js")
 
 
 function plcOnClick() {
-    let newWin = window.open("about:blank", "hello", "width=200,height=200");
-    newWin.document.write("Hello, world!");
+    let newWin = window.open("PLC Integration", "hello", "width=200,height=200");
+    newWin.document.write("works great with PLCs!");
 }
