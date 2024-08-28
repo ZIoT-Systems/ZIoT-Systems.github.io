552df33db3d5ff471ae0008bec2d59fdb3ae09f9
jglatts <john@z-axiscc.com>
2024-08-28 13:51:56 -0400

    updates Wed 08/28/2024 13:51:56.90

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,1 +1,7 @@
 console.log("hello from loaded js")
+
+
+function plcOnClick() {
+    let newWin = window.open("about:blank", "hello", "width=200,height=200");
+    newWin.document.write("Hello, world!");
+}
