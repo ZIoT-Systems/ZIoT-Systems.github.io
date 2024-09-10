4abacb8c5355a3fd2c13007aa5e1b5b8c7abae75
jglatts <john@z-axiscc.com>
2024-09-10 10:24:04 -0400

    updates Tue 09/10/2024 10:24:04.67

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,4 +1,5 @@
-console.log("hello from loaded js")
+console.log("hello from loaded js");
+console.log("hello from loaded js - this is a test");
 
 /*
  *  not working well IOS, fix! 
@@ -6,4 +7,17 @@ console.log("hello from loaded js")
 function plcOnClick() {
     let newWin = window.open("/", "hello", "width=200,height=200");
     newWin.document.write("ZIoT Systems offers an advanced yet straightforward solution for integrating with your Programmable Logic Controllers (PLCs).");
+}
+
+function myHide(str) {
+    let ele = document.getElementById(str);
+    if (window.getComputedStyle(ele).display === "none") {
+        // show the element
+        ele.style.display = "block";
+    }
+    else {
+        // hide the element
+        ele.style.display = "none";
+    }
+    console.log("got a click!");
 }
