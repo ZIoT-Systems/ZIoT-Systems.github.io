f69a93ff1e6a57fc1ca3fe3c17e82b9c0c523b50
jglatts <john@z-axiscc.com>
2024-11-18 20:52:04 -0500

    updates Mon 11/18/2024 20:52:04.25

--- assets/js/typer.js
+++ assets/js/typer.js
@@ -1,2 +1,11 @@
-// JavaScript source code
-console.log("hello")
+var i = 0;
+var txt = 'Advanced Industrial Monitoring'; /* The text */
+var speed = 100; 
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("typer1").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
