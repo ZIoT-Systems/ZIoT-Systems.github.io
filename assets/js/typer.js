0ea44da4c7978260d5c4000d92b103cdd1baedc7
jglatts <john@z-axiscc.com>
2024-11-19 08:30:03 -0500

    updates Tue 11/19/2024  8:30:02.94

--- assets/js/typer.js
+++ assets/js/typer.js
@@ -3,14 +3,18 @@ var txt = 'Advanced Industrial Monitoring'; /* The text */
 var speed = 100; 
 
 function typeWriter() {
-    if (i < txt.length) {
+    if (i == -1) {
+        document.getElementById("typer1").innerHTML = "";
+        i = 0;
+        setTimeout(typeWriter, speed);
+    }
+    else if (i < txt.length) {
         document.getElementById("typer1").innerHTML += txt.charAt(i);
         i++;
         setTimeout(typeWriter, speed);
     }
     else {
-        document.getElementById("typer1").innerHTML = "";
-        i = 0;
+        i = -1;
         setTimeout(typeWriter, 10000);
     }
 }
