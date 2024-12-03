7d2146df8a5472264b4c07eb69a569ab8954a10c
jglatts <john@z-axiscc.com>
2024-12-02 21:20:15 -0500

    updates Mon 12/02/2024 21:20:15.56

--- assets/js/typer.js
+++ assets/js/typer.js
@@ -1,11 +1,22 @@
 var i = 0;
-var txt = 'Advanced Industrial Monitoring'; 
+var str_idx = 0;
+var all_txt = [
+    'Advanced Industrial Monitoring',
+    'Empowering Smarter Operations',
+    'Real-Time Data, Real Results',
+    'Insightful. Reliable. Scalable.',
+   ]; 
 
 function typeWriter() {
+    var txt = all_txt[str_idx];    
     var speed = 100;
     if (i == -1) {
         document.getElementById("typer1").innerHTML = "";
         i = 0;
+        if (str_idx < 4)
+            str_idx++;
+        else
+            str_idx = 0;
     }
     else if (i < txt.length) {
         document.getElementById("typer1").innerHTML += txt.charAt(i);
